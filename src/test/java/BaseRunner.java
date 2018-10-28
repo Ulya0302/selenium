8a625be142605e530d7a4e219619939b447939ba
Ulya <tellera111@gmail.com>
2018-10-28 12:13:42 +0300

    Add opera supports

--- src/test/java/BaseRunner.java
+++ src/test/java/BaseRunner.java
@@ -1,23 +1,37 @@
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseRunner {
     WebDriver driver;
     String baseUrl;
+    String browserName;// = System.getProperty("browser");
 
     @Before
     public void setUp() {
-        driver = BrowseFactory.chrome.create();
+        driver = getDriver();
+        //driver = BrowseFactory.chrome.create();
         driver.manage().window().maximize();
         baseUrl = "https://moscow-job.tinkoff.ru/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
+
     @After
     public void tearDown() {
         driver.quit();
     }
 
+    private WebDriver getDriver() {
+        try {
+            browserName = System.getProperty("browser");
+            BrowseFactory.valueOf(browserName);
+        } catch (NullPointerException | IllegalArgumentException e) {
+            System.setProperty("browser", "chrome");
+            browserName = System.getProperty("browser");
+        }
+        return BrowseFactory.valueOf(browserName).create();
+    }
 }
--- src/test/java/BrowseFactory.java
+++ src/test/java/BrowseFactory.java
@@ -1,20 +1,45 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
 
 public enum  BrowseFactory {
         chrome {
             public WebDriver create() {
+                updateProperty("chrome");
                 return new ChromeDriver();
             }
         },
         firefox {
             public WebDriver create() {
+                updateProperty("firefox");
                 return new FirefoxDriver();
             }
+        },
+        opera {
+            public WebDriver create() {
+                updateProperty("opera");
+                //"C:\\Program Files\\Opera\\56.0.3051.43\\opera.exe"
+                OperaOptions opt = new OperaOptions();
+                String path = System.getProperty("operaBinary");
+                System.out.println(path);
+                if (path == null) {
+                    System.setProperty("operaBinary", "C:\\Program Files\\Opera\\56.0.3051.52\\opera.exe");
+                }
+                opt.setBinary(System.getProperty("operaBinary"));
+
+                return new OperaDriver(opt);
+            }
+
         };
         public WebDriver create() {
             return null;
+        }
+
+        public void updateProperty(String browserName) {
+            if (System.getProperty("browser") == null)
+            System.setProperty("browser", browserName);
         }
     }
 
