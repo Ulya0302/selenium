487eec5e60a37dff480f385e6ee37b9cb5d9d737
Ulya <tellera111@gmail.com>
2018-10-28 12:30:49 +0300

    Update gitignore

--- src/test/java/BaseRunner.java
+++ src/test/java/BaseRunner.java
@@ -8,12 +8,11 @@ import java.util.concurrent.TimeUnit;
 public class BaseRunner {
     WebDriver driver;
     String baseUrl;
-    String browserName;// = System.getProperty("browser");
+    String browserName;
 
     @Before
     public void setUp() {
         driver = getDriver();
-        //driver = BrowseFactory.chrome.create();
         driver.manage().window().maximize();
         baseUrl = "https://moscow-job.tinkoff.ru/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
--- src/test/java/BrowseFactory.java
+++ src/test/java/BrowseFactory.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.opera.OperaOptions;
 
@@ -8,19 +10,24 @@ public enum  BrowseFactory {
         chrome {
             public WebDriver create() {
                 updateProperty("chrome");
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--disable-notifications");
                 return new ChromeDriver();
             }
         },
         firefox {
             public WebDriver create() {
                 updateProperty("firefox");
+                System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+                System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "./src/test/java/firefox.log");
+                FirefoxOptions options = new FirefoxOptions();
+                options.addPreference("dom.webnotifications.enabled", false);
                 return new FirefoxDriver();
             }
         },
         opera {
             public WebDriver create() {
                 updateProperty("opera");
-                //"C:\\Program Files\\Opera\\56.0.3051.43\\opera.exe"
                 OperaOptions opt = new OperaOptions();
                 String path = System.getProperty("operaBinary");
                 System.out.println(path);
